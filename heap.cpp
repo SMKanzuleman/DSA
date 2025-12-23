afc95d0df95a6751121c6a1ce62b25a31bae91df
SMKanzulEman <syedkanzuleman.official@gmail.com>
2025-12-23 20:27:05 +0500

    Delete Function Added

--- heap.cpp
+++ heap.cpp
@@ -1,35 +1,74 @@
 #include <iostream>
 using namespace std;
-class Heap{
+class Heap
+{
     int arr[100];
     int size;
-    public:
-    Heap(){
-        size=0;
+
+public:
+    Heap()
+    {
+        size = 0;
     }
-    void insert(int key){
-        int i=size;
-        arr[size++]=key;
-        while(i>0){
-            int parent=(i-1)/2;
-            if(arr[parent]<arr[i]){
-                swap(arr[parent],arr[i]);
-                i=parent;
+    void insert(int key)
+    {
+        int i = ++size;
+        arr[i] = key;
+        while (i > 1)
+        {
+            int parent = i / 2;
+            if (arr[parent] < arr[i])
+            {
+                swap(arr[parent], arr[i]);
+                i = parent;
             }
-            else{
+            else
+            {
                 return;
             }
         }
     }
-    void print(){
-        for(int i=0;i<size;i++){
-            cout<<arr[i]<<" ";
+    void delNode()
+    {
+        arr[1] = arr[size];
+        int i = 1;
+        size--;
+        while (i <= size)
+        {
+            int leftChld = 2 * i;
+            int rightChld = 2 * i + 1;
+            int largest=i;
+            if (leftChld <= size && arr[leftChld] > arr[rightChld])
+            {
+                largest = leftChld;
+            }
+            if (rightChld <= size && arr[rightChld] > arr[leftChld])
+            {
+                largest = rightChld;
+            }
+            if (largest!=i)
+            {
+                swap(arr[i], arr[largest]);
+                i = largest;
+            }
+            else
+            {
+                break;
+            }
         }
-        cout<<endl;
+    }
+    void print()
+    {
+        for (int i = 1; i <= size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
     }
 };
 
-int main() {
+int main()
+{
     Heap h;
     h.insert(50);
     h.insert(40);
@@ -39,6 +78,8 @@ int main() {
     h.insert(15);
     h.insert(28);
     h.insert(75);
+    h.print();
+    h.delNode();
     h.print();
 
     cout << "Hi KanzulEman" << endl;
