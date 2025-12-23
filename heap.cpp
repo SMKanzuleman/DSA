0ee06b5bd42c1b8ccb6f4e05311a109adc60219d
SMKanzulEman <syedkanzuleman.official@gmail.com>
2025-12-23 19:43:20 +0500

    Insert Funcatinon added successfully

--- heap.cpp
+++ heap.cpp
@@ -12,24 +12,34 @@ class Heap{
         arr[size++]=key;
         while(i>0){
             int parent=(i-1)/2;
-            if(arr[parent<arr[i]]){
+            if(arr[parent]<arr[i]){
                 swap(arr[parent],arr[i]);
                 i=parent;
             }
             else{
                 return;
             }
-
         }
-
-
-
-
     }
-    
+    void print(){
+        for(int i=0;i<size;i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
 };
 
 int main() {
+    Heap h;
+    h.insert(50);
+    h.insert(40);
+    h.insert(30);
+    h.insert(20);
+    h.insert(10);
+    h.insert(15);
+    h.insert(28);
+    h.insert(75);
+    h.print();
 
     cout << "Hi KanzulEman" << endl;
     return 0;
