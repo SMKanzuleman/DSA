cee90ea73c76132e564f1dd4578f6b6648bd6f07
SMKanzulEman <syedkanzuleman.official@gmail.com>
2025-12-23 19:38:54 +0500

    Basic Code

--- heap.cpp
+++ heap.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
 using namespace std;
-
 class Heap{
     int arr[100];
-    int size=-1;
+    int size;
     public:
     Heap(){
         size=0;
     }
     void insert(int key){
+        int i=size;
         arr[size++]=key;
+        while(i>0){
+            int parent=(i-1)/2;
+            if(arr[parent<arr[i]]){
+                swap(arr[parent],arr[i]);
+                i=parent;
+            }
+            else{
+                return;
+            }
+
+        }
+
+
+
+
     }
+    
 };
 
 int main() {
