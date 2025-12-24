8ebc874ce76cabc233329122c0d7e7e5122e3e52
SMKanzulEman <syedkanzuleman.official@gmail.com>
2025-12-25 03:52:29 +0500

    Improved Logic

--- quickSort--random.cpp
+++ quickSort--random.cpp
@@ -1,47 +1,85 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-int randomPartition(vector<int> &arr, int start, int end)
+int hoarePartition(int arr[], int L, int H)
 {
-    int randomIndex = start + rand() % (end - start + 1);
-    cout<<"Random pivot choosed :"<<randomIndex<<endl;
-    // swap random pivot with last element
-    swap(arr[randomIndex], arr[end]);
-
-    int i = start - 1;
-    int x = arr[end]; // pivot
-    for (int j = start; j < end; j++)
+    int pivot = arr[L];
+    int i = L - 1;
+    int j = H + 1;
+    while (true)
     {
-        if (arr[j] < x) // comparing pivot with array indexes
+        do
+        {
+            i++;
+        } while (arr[i] < pivot);
+        do
+        {
+            j--;
+        } while (arr[j] > pivot);
+
+        if (i >= j)
+        {
+            return j;
+        }
+        swap(arr[i], arr[j]);
+    }
+}
+int lumotoPartition(int arr[], int L, int H)
+{
+    int pivot = arr[H];
+    int i = L - 1;
+    for (int j = L; j < H; j++)
+    {
+        if (arr[j] < pivot)
         {
             i++;
             swap(arr[i], arr[j]);
         }
     }
     i++;
-    swap(arr[i], arr[end]);
+    swap(arr[i], arr[H]);
     return i;
 }
-void quickSort(vector<int> &arr, int start, int end)
+int lumotoRandom(int arr[], int L, int H)
 {
-    if (start < end)
+    int randomIndex = L + rand() % (H - L + 1);
+    cout<<"Random pivot choosed :"<<randomIndex<<endl;
+    // swap random pivot with last element
+    swap(arr[randomIndex], arr[H]);
+    return lumotoPartition(arr, L, H);
+
+}
+int HoarsRandom(int arr[], int L, int H)
+{
+    int randomIndex = L + rand() % (H - L + 1);
+    cout << "Random pivot choosed :" << randomIndex << endl;
+    // swap random pivot with last element
+    swap(arr[randomIndex], arr[H]);
+    return hoarePartition(arr, L, H);
+}
+
+void quickSort(int arr[], int L, int H)
+{
+    if (L < H)
     {
-        int pi = randomPartition(arr, start, end);
-        quickSort(arr, start, pi - 1);// subArray conatining values lesser than pivot
-        quickSort(arr, pi + 1, end);  // subArray conatining  values greater than pivot
+        int pi = HoarsRandom(arr, L, H);
+        quickSort(arr, L, pi - 1);// subArray conatining values lesser than pivot
+        quickSort(arr, pi + 1, H);  // subArray conatining  values greater than pivot
     }
 }
-void printVector(const vector<int> &v)
+void printArray(int arr[], int n)
 {
-    for (int x : v)
+    for (int i = 0; i < n; i++)
     {
-        cout << x << " ";
+        cout << arr[i] << " ";
     }
     cout << "\n";
 }
+
 int main()
 {
-    vector<int> test = {6, 8, 23, 21, 62, 72, 20, 2};
-    quickSort(test, 0, test.size() - 1);
-    printVector(test);
+    int test[] = {20, 10, 40, 30, 50};
+    int size = sizeof(test) / sizeof(test[0]);
+    cout << size << endl;
+    quickSort(test, 0, size - 1);
+    printArray(test, size);
 }
