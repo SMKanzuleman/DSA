b70dcf45d318ad6a934d1b5f73700f9a56707410
SMKanzulEman <syedkanzuleman.official@gmail.com>
2025-12-25 03:51:44 +0500

    Improved Logic

--- quickSort--lumoto.cpp
+++ quickSort--lumoto.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int lumotoPartition(int arr[], int L, int H)
 {
-    int i = L ;
-    int pivot = arr[H]; // pivot
+    int pivot = arr[H];
+    int i = L - 1;
     for (int j = L; j < H; j++)
     {
-        if (arr[j] < pivot) // comparing pivot with array indepivotes
+        if (arr[j] < pivot)
         {
             i++;
             swap(arr[i], arr[j]);
@@ -21,12 +21,14 @@ void quickSort(int arr[], int L, int H)
     if (L < H)
     {
         int pi = lumotoPartition(arr, L, H);
-        quickSort(arr, L, pi - 1);// subArray conatining values lesser than pivot
-        quickSort(arr, pi + 1, H);  // subArray conatining  values greater than pivot
+        quickSort(arr, L, pi - 1); // subArray conatining values lesser than pivot
+        quickSort(arr, pi + 1, H); // subArray conatining  values greater than pivot
     }
 }
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         cout << arr[i] << " ";
     }
     cout << "\n";
@@ -34,7 +36,7 @@ void printArray(int arr[], int n) {
 int main()
 {
     int test[] = {6, 8, 23, 21, 62, 72, 20, 2};
-    int size= sizeof(test) / sizeof(test[0]);
+    int size = sizeof(test) / sizeof(test[0]);
     quickSort(test, 0, size - 1);
     printArray(test, size);
 }
